74470b9e6db67ab2a7067ef625bd2dda9ca7863a
Rap-L <keshu22kesu@gmail.com>
2025-01-11 11:45:56 +0530

    Create cuboid.py

---
+++ new/graphics/subgraphics/cuboid.py
@@ -0,0 +1,8 @@
+def lsa(l,b,h):
+    print("The lateral surface area of cuboid is: ",2*(l+b)*h)
+
+def tsa(l,b,h):
+    print("The total surface area of cuboid is: ",2*(l*b +b*h +l*h))
+
+def cvol(l,b,h):
+    print("The volume of cuboid is: ",l*b*h)
