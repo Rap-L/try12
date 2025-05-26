69093a672e4fdbd335a9691d5627dc6489841b03
Rap-L <keshu22kesu@gmail.com>
2025-05-26 19:36:07 +0530

    Update filereg.sh

--- network/filereg.sh
+++ network/filereg.sh
@@ -1,3 +1,7 @@
+#Write a shell script that prompts the user for a name of a file or
+#directory and reports if it is a regular file, a directory, or another type of
+#file. Also perform an ls command against the file or directory with the
+#long listing option.
 #!/bin/bash
 
 echo “Enter the file path”
