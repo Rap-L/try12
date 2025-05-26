4976eca2b44f876104730b42568b2c493d90115d
Rap-L <keshu22kesu@gmail.com>
2025-05-26 19:44:20 +0530

    Create dltzerofile.sh

---
+++ network/dltzerofile.sh
@@ -0,0 +1,30 @@
+#Prompt the name of the folder and delete all
+#subfolders with file size 0
+
+#! /bin/bash
+
+echo -n "Enter name of the directory : "
+
+read directory_name
+
+if [ -d "$directory_name" ]; then
+
+echo "Directory exist"
+
+for i in `find $directory_name -size 0`
+
+do
+
+rm $i
+
+rm -rf $i
+
+echo "Zero-sized files are Successfully deleted"
+
+done
+
+else
+
+echo "Directory does not exist"
+
+fi
