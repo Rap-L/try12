9e24e69b2064d7e1e242c07b4f51dbfd77337460
Rap-L <keshu22kesu@gmail.com>
2025-05-26 19:34:47 +0530

    Create nodirfile.sh

---
+++ network/nodirfile.sh
@@ -0,0 +1,20 @@
+#Write a shell script that consists of a function that displays the
+#number of files in the present working directory. Name this function
+#“file_count” and call it in your script. If you use variable in your
+#function, remember to make it a local variable.
+
+#!/bin/bash
+
+function count_items() {
+
+local files=$(find . -maxdepth 1 -type f | wc -l)
+
+local dirs=$(find . -maxdepth 1 -type d | wc -l)
+
+echo "Files: $files"
+
+echo "Directories: $dirs"
+
+}
+
+count_items
