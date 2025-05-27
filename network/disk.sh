032c9b672ccc8eb97c0524b94ffcd76d80deabe8
Rap-L <keshu22kesu@gmail.com>
2025-05-27 12:45:27 +0530

    Update disk.sh

--- network/disk.sh
+++ network/disk.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 Threshold=80
-Usage=$(df / | grep/ |awk '{print $5}'  | sed 's/%//g')
+Usage=$(df / | grep / |awk '{print $5}'  | sed 's/%//g')
 if [ "$Usage" -gt "$Threshold" ]; then
-    echo "warning:disk usage is at {$Usage}% !"
+    echo "warning:disk usage is at $Usage% !"
 else
-    echo "Disk usage is normal:{$Usage} %"
+    echo "Disk usage is normal:$Usage%"
 fi
+#output
+@Rap-L ➜ /workspaces/try12 (s2) $ ./arthim.sh
+Disk usage is normal:36%
+@Rap-L ➜ /workspaces/try12 (s2) $ 
