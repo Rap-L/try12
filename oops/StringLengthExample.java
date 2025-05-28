32e4ef51aa40aa1c68167a751fee9fef45e609c8
Rap-L <keshu22kesu@gmail.com>
2025-05-28 12:54:40 +0530

    Add files via upload

---
+++ oops/StringLengthExample.java
@@ -0,0 +1,8 @@
+public class StringLengthExample{
+	public static void main(String[] args){
+		String str="Hello,World!";
+		System.out.println("length of the string:"+str.length());
+		System.out.println("Charater at index 7:"+str.charAt(7));
+		}
+		
+}
